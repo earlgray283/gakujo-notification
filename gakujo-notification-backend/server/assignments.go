672deeb796f617cf7c0a2d1e941a861bd712369c
earlgray <earlgray283@gmail.com>
2022-07-30 03:50:22 +0900

    fix: add status field

--- gakujo-notification-backend/server/assignments.go
+++ gakujo-notification-backend/server/assignments.go
@@ -1,6 +1,7 @@
 package server
 
 import (
+	"gakujo-notification/gakujo"
 	"gakujo-notification/repository"
 
 	"net/http"
@@ -8,6 +9,11 @@ import (
 	"github.com/gofiber/fiber/v2"
 	"github.com/golang-jwt/jwt/v4"
 )
+
+type Assignment struct {
+	repository.Assignment
+	Status gakujo.AssignmentStatus `json:"status"`
+}
 
 func (srv *Server) HandleGetAllAssignments(c *fiber.Ctx) error {
 	year, _ := c.ParamsInt("year", 2022)
@@ -17,9 +23,12 @@ func (srv *Server) HandleGetAllAssignments(c *fiber.Ctx) error {
 		srv.logger.Println(err)
 		return c.SendStatus(http.StatusInternalServerError)
 	}
-	assignments := make([]*repository.Assignment, len(userAssignments))
+	assignments := make([]*Assignment, len(userAssignments))
 	for i, userAssignment := range userAssignments {
-		assignments[i] = &userAssignment.Assignment
+		assignments[i] = &Assignment{
+			Assignment: userAssignment.Assignment,
+			Status:     userAssignment.Status,
+		}
 	}
 	return c.Status(http.StatusOK).JSON(assignments)
 }
